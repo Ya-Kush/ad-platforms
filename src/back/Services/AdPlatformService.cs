2b1a375eee5467df0f25da5b1f80616e2c901460
Yaroslav Kushnirenko <kushnirenko.ya@yandex.ru>
2025-09-23 23:37:56 +0300

    Improved the ad platform service

--- src/back/Services/AdPlatformService.cs
+++ src/back/Services/AdPlatformService.cs
@@ -16,25 +16,74 @@ public sealed class AdPlatformService
     readonly static Lock _lock = new();
     static FrozenDictionary<Location, FrozenSet<AdPlatform>>? _platforms;
 
-    public Result TryParseAndLoad(string data) => Result.Try(() =>
-    {
-        lock (_lock) _platforms = data.Split('\n')
-            .Select((l, i) => l.Split(':') is [var platform, var paths]
-                ? new AdPlatform(platform, paths.Split(',').Select(p => new Location(p)))
-                : throw new ParseException($"The wrong format of data. Occurred in {i}th row"))
-            .SelectMany(p => p.Locations, (p, l) => (location: l, platform: p))
-            .GroupBy(lp => lp.location, lp => lp.platform)
-            .ToFrozenDictionary(group => group.Key, group => group.ToFrozenSet());
-    });
-
     public Result<IEnumerable<AdPlatform>> FindAtLocation(Location location)
     {
         lock (_lock) return _platforms is { }
-            ? _platforms.TryGetValue(location, out var value) ? value: new NotFoundException()
+            ? _platforms.TryGetValue(location, out var value) ? value : new NotFoundException()
             : new UninitializedException();
     }
-
     public Result<IEnumerable<AdPlatform>> FindAtLocation(string path) => Result.Try(
         () => new Location(path)).Match(FindAtLocation,
             e => e is ModelException ? new ValidationException(e.Message, e) : e);
+
+    public Result TryParseAndLoad(string data)
+    {
+        lock (_lock)
+        {
+            var res = Result.Try(() => { _platforms = ParseInlineData(data).ToFrozenDictionary(g => g.Key, g => g.Value.ToFrozenSet()); });
+            return res.Failure && res.Exception is ModelException e
+                ? new ValidationException(e.Message, e) : res;
+        }
+    }
+
+    static StringSplitOptions TrimAndSift => StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries;
+    internal static Dictionary<Location, HashSet<AdPlatform>> ParseInlineData(string data)
+    {
+        var lines = data.Split(['\n', '\r'], TrimAndSift);
+        if (lines is []) throw new ValidationException("Data was blank");
+
+        Dictionary<Location, HashSet<AdPlatform>> res = new(lines.Length);
+        void add(Location l, AdPlatform p) => (res.TryGetValue(l, out var set) ? set : res[l] = []).Add(p);
+
+        var adPlatformsAt = lines
+            .Select(parseDataLineToAdPlatform)
+            .SelectMany(p => p.locations, (p, l) => (location: l, p.platform))
+            .GroupBy(lp => lp.location, lp => lp.platform)
+            .ToFrozenDictionary(g => g.Key, g => g.ToFrozenSet());
+
+        var locations = ParseAndGroupLocations(adPlatformsAt.Keys);
+
+        foreach (var (location, subs) in locations)
+        foreach (var sub in subs)
+        foreach (var platform in adPlatformsAt.TryGetValue(location, out var platforms) ? platforms : [])
+            add(sub, platform);
+
+        return res;
+
+        static (AdPlatform platform, IEnumerable<Location> locations) parseDataLineToAdPlatform(string line, int i)
+            => line.Split(':', TrimAndSift) is [var platform, var paths]
+            ? (new(platform), paths.Split(',', TrimAndSift).Select(p => new Location(p)))
+            : throw new ParseException($"The wrong format of data. Occurred in {i}th row");
+    }
+
+    internal static IReadOnlyDictionary<Location, IReadOnlySet<Location>> ParseAndGroupLocations(IEnumerable<Location> locations)
+    {
+        Dictionary<Location, HashSet<Location>> res = [];
+        foreach (var location in locations)
+        {
+            if (res.ContainsKey(location)) continue;
+            var generals = parseLocation(location).ToArray();
+            for (var i = 1; i <= generals.Length; i++)
+            for (var j = i; j <= generals.Length; j++)
+                add(generals[^i], generals[^j]);
+        }
+        return res.ToFrozenDictionary(d => d.Key, d => (IReadOnlySet<Location>)d.Value.ToFrozenSet()).AsReadOnly();
+
+        void add(Location l, Location p) => (res.TryGetValue(l, out var set) ? set : res[l] = []).Add(p);
+        static IEnumerable<Location> parseLocation(Location location)
+        {
+            for (var i = location.Path.Length; i > 0; i = location.Path.LastIndexOf('/', i - 1))
+                yield return new Location(location.Path[0..i]);
+        }
+    }
 }
