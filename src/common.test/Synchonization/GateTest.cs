4e6520d2e74eae8f79934cb4289724c3063d12e7
Yaroslav Kushnirenko <kushnirenko.ya@yandex.ru>
2025-09-26 01:29:03 +0300

    Implemented an async synchronization primitive with its tests

---
+++ src/common.test/Synchonization/GateTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using AdPlatforms.Common.Synchronization;
+
+namespace AdPlatforms.Common.Synchonization.Test;
+
+public sealed class GateTest
+{
+    [Fact]
+    public async Task Cross_Opened()
+    {
+        var gate = new Gate();
+        var keys = new string[] { "a", "b" };
+        await Assert.AllAsync(keys, k => gate.CrossAsync(k).AsTask());
+    }
+
+    [Fact]
+    public async Task Cross_Closing()
+    {
+        var gate = new Gate();
+        var keys = new string[] { "a", "b" };
+
+        await gate.CloseAsync(this);
+        await Task.Delay(10);
+        var tasks = keys.Select(k => gate.CrossAsync(k).AsTask()).ToArray();
+        Assert.Equal([false, false], tasks.Select(t => t.IsCompleted));
+
+        await gate.OpenAsync(this);
+        await Task.Delay(10);
+        Assert.Equal([true, true], tasks.Select(t => t.IsCompleted));
+    }
+
+    [Fact]
+    public async Task Cross_CloserOnly()
+    {
+        var gate = new Gate();
+        var keys = new string[] { "a", "b" };
+
+        await gate.CloseAsync(keys[0]);
+        await Task.Delay(10);
+        var tasks = keys.Select(k => gate.CrossAsync(k).AsTask()).ToArray();
+        Assert.Equal([true, false], tasks.Select(t => t.IsCompleted));
+    }
+
+    [Fact]
+    public async Task ScopedClose()
+    {
+        var gate = new Gate();
+        var keys = new string[] { "a", "b" };
+        Task[] tasks;
+        await using (var s = await gate.ScopedCloseAsync())
+        {
+            await Task.Delay(10);
+            tasks = keys.Select(k => gate.CrossAsync(k).AsTask()).ToArray();
+            Assert.Equal([false, false], tasks.Select(t => t.IsCompleted));
+    
+            var t = gate.CrossAsync(s);
+            await Task.Delay(10);
+            Assert.True(t.IsCompleted);
+        }
+        Assert.Equal([true, true], tasks.Select(t => t.IsCompleted));
+    }
+}
